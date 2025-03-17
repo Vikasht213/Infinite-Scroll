7222a306cac9e1c329482e82f96e92ab02ade999
VIKASH TIWARI <tvikash964@gmail.com>
2025-03-17 23:05:09 +0530

    build resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect, useRef, useCallback } from "react"
-import ImageCard from "./components/ImageCard"
+import ImageCard from "./components/Imagecard"
 import "./index.css"
 
 function App() {
